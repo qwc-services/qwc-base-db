119b21006d509c9029ef9ff8cfe9a3f84cbf47f0
Benoît Blanc <benoit.blanc@oslandia.com>
2023-10-06 08:32:25 +0200

    Use virtual env to install requirements

--- install-alembic-and-clone-qwc-config-db.sh
+++ install-alembic-and-clone-qwc-config-db.sh
@@ -15,11 +15,13 @@ QWC_CONFIG_DB_GIT_REPO="$1"
 QWC_CONFIG_DB_DEST="$2"
 
 apt-get install -y ca-certificates tmux screen curl less \
-                   git python3-pip python3-psycopg2 gdal-bin
+                   git python3-pip python3-psycopg2 python3-venv libpq-dev gdal-bin
 
 # get qwc-config-db for migrations
 cd /tmp/ && git clone $QWC_CONFIG_DB_GIT_REPO qwc-config-db
 cd /tmp/qwc-config-db/ && git pull
-pip3 install --upgrade pip
-pip3 install --no-cache-dir -r /tmp/qwc-config-db/requirements.txt
+python3 -m venv .venv
+source .venv/bin/activate
+python3 -m pip install --upgrade pip
+python3 -m pip install --no-cache-dir -r /tmp/qwc-config-db/requirements.txt
 
--- run-migrations.sh
+++ run-migrations.sh
@@ -3,4 +3,5 @@ set -e
 
 # run migrations from qwc-config-db
 cd /tmp/qwc-config-db/
+source .venv/bin/activate
 PGSERVICE=qwc_configdb alembic upgrade ${ALEMBIC_VERSION:-head}
