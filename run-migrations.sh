b2d58fdbdc66a80ffa0d0e3263b285c8bb87708f
Sandro Mani <manisandro@gmail.com>
2024-07-24 17:03:53 +0200

    Honour the PGSERVICE ENV-var in run-migrations.shy

--- run-migrations.sh
+++ run-migrations.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-until psql service=qwc_configdb -c "select 1" > /dev/null 2>&1; do
+until psql service=$PGSERVICE -c "select 1" > /dev/null 2>&1; do
   echo "Waiting for postgres server..."
   sleep 1
 done
